f2f756ed49b0c08542bfff0d4f15184df40446f2
OlolaJaco <akinbusolaakinola2@gmail.com>
2025-03-31 01:10:01 +0100

    fix

--- Backend/config/upstash.js
+++
@@ -1,6 +0,0 @@
-import { Client as WorkflowClient } from '@upstash/workflow';
-
-export const WorkflowClient = new WorkflowClient( {
-    baseUrl: process.env.QSTASH_URL,
-    token: process.env.QSTASH_TOKEN
-});
